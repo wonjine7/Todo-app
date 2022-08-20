ddd625a9168c8378e4b2ab1789097588fb20b9fc
wonjine7 <wonjine7@naver.com>
2022-08-20 15:48:31 +0900

    react-virtualized를 사용한 렌더링 최적화

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -8,10 +8,11 @@ import cn from 'classnames';
 import './TodoListItem.scss';
 
 
-const TodoListItem = ({todo, onRemove, onToggle}) => {
+const TodoListItem = ({todo, onRemove, onToggle, style}) => {
   const {id,text, checked}= todo;
   return (
-    <div className='TodoListItem'>
+    <div className='TodoListItem-virtualized' style={style}>
+      <div className='TodoListItem'>
       <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
       {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className='text'>{text}</div>
@@ -20,11 +21,13 @@ const TodoListItem = ({todo, onRemove, onToggle}) => {
         <MdRemoveCircleOutline />
       </div>
     </div>
+    </div>
   );
 };
 
 
 
-export default React.memo(TodoListItem);
+export default React.memo(TodoListItem, 
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,);
 // React.memo() 컴포넌트의 props가 바뀌지 않았다면,
 // 리렌더링하지 않도록 설정하여 함수형 컴포넌트의 리렌더링 성능을 최적화해 줄 수 있다.
